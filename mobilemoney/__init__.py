bcbf2cc00e3ad4f5b68fc602272f3edba2f6f543
Muhammed Zeba <parice02@hotmail.com>
2023-11-10 19:00:24 +0000

    add ligdicash

--- mobilemoney/__init__.py
+++ mobilemoney/__init__.py
@@ -10,6 +10,10 @@ from mobilemoney.moovmoney import (
     Payment as MMPayment,
     DevPayment as MMDevPayment,
 )
+from mobilemoney.ligdicash import (
+    GenericPaymentWithRedirect as LigdicashGenericPaymentWithRedirect,
+    Payment as LigdicashPaymentWithRedirect,
+)
 
 
 def validate_om_prod_payment(
@@ -36,3 +40,62 @@ def validate_om_dev_payment(
 ):
     payment = OMDevPayment(phonenumber, username, password)
     return payment.validate_payment(customer_phone, customer_otp, amount, message)
+
+
+def validate_ligdicash_payment(api_key, api_token, command):
+    """
+    Validate a payment
+        - command param must be :
+            {
+                "invoice": {
+                    "items": [
+                    {
+                        "name": "Nom du produit ou Service",
+                        "description": " Description du produit ou Service ",
+                        "quantity": 1,
+                        "unit_price": 100,
+                        "total_price": 100
+                    }
+                    ],
+                    "total_amount": 100,
+                    "devise": "XOF",
+                    "description": " Description du contenu de la facture(Achat de jus de fruits)",
+                    "customer": "",
+                    "customer_firstname": "Nom du client",
+                    "customer_lastname": "Prénom du client",
+                    "customer_email": "tester@gligdicash.com",
+                    "external_id": "",
+                    "otp": ""
+                },
+                "store": {
+                    "name": "Nom de votre site ou de votre boutique",
+                    "website_url": "url de votre site ou de votre boutique"
+                },
+                "actions": {
+                    "cancel_url": "http://localhost",
+                    "return_url": "http://localhost",
+                    "callback_url": "http://localhost"
+                },
+                "custom_data": {
+                    "transaction_id": "2021000000001",
+                    "logfile": "202110210048426170b8ea884a9",
+                    "developpeur": "kaboretidiane"
+                }
+            }
+
+        - response will be:
+            {
+                "response_code": "00",
+                "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF9pbnZvaWNlIjoiMTU0NTkwMTIiLCJzdGFydF9kYXRlIjoiMjAyMy0wMi0yMyAxODoyNDoxMiIsImV4cGlyeV9kYXRlIjoxNjc3MjU5NDUyfQ.U9GwpBQpZwW1YdWD_0Zla9-Uy-sUV7zDn4Nobtfc33M",
+                "response_text": "https://client.ligdicash.com/directpayment/invoice/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF9pbnZvaWNlIjoiMTU0NTkwMTIiLCJzdGFydF9kYXRlIjoiMjAyMy0wMi0yMyAxODoyNDoxMiIsImV4cGlyeV9kYXRlIjoxNjc3MjU5NDUyfQ.U9GwpBQpZwW1YdWD_0Zla9-Uy-sUV7zDn4Nobtfc33M",
+                "description": "",
+                "custom_data": {
+                    "transaction_id": "2021000000001",
+                    "logfile": "2023022318241263f7af4ccc792",
+                    "developpeur": "kaboretidiane",
+                },
+                "wiki": "https://client.ligdicash.com/wiki/createInvoice",
+            }
+    """
+    payment = LigdicashPaymentWithRedirect(username=api_key, password=api_token)
+    return payment.validate_payment(command)
---
+++ mobilemoney/ligdicash.py
@@ -0,0 +1,110 @@
+from typing import Any, List, Union, Dict
+
+import requests  # TODO replace with urllib3
+from requests.auth import HTTPBasicAuth
+
+from .base import BasePayment
+
+ligdicash_dev_url_with_redirect = (
+    "https://app.ligdicash.com/pay/v01/redirect/checkout-invoice/create"
+)
+ligdicash_prod_url_with_redirect = (
+    "https://app.ligdicash.com/pay/v01/redirect/checkout-invoice/create"
+)
+
+
+class GenericPaymentWithRedirect(BasePayment):
+    def __init__(self, url="", username="", password=""):
+        super().__init__(None, username, password)
+
+        if not isinstance(url, str):
+            raise ValueError("value 'url' must be type of 'str'")
+        self._url = url
+
+    def validate_payment(self, command={}, verify_ssl=False):
+        """
+        Validate a payment
+            - command param must be :
+                {
+                    "invoice": {
+                        "items": [
+                        {
+                            "name": "Nom du produit ou Service",
+                            "description": " Description du produit ou Service ",
+                            "quantity": 1,
+                            "unit_price": 100,
+                            "total_price": 100
+                         }
+                      ],
+                        "total_amount": 100,
+                        "devise": "XOF",
+                        "description": " Description du contenu de la facture(Achat de jus de fruits)",
+                        "customer": "",
+                        "customer_firstname": "Nom du client",
+                        "customer_lastname": "Prénom du client",
+                        "customer_email": "tester@gligdicash.com",
+                        "external_id": "",
+                        "otp": ""
+                    },
+                    "store": {
+                        "name": "Nom de votre site ou de votre boutique",
+                        "website_url": "url de votre site ou de votre boutique"
+                    },
+                    "actions": {
+                        "cancel_url": "http://localhost",
+                        "return_url": "http://localhost",
+                        "callback_url": "http://localhost"
+                    },
+                   "custom_data": {
+                        "transaction_id": "2021000000001",
+                        "logfile": "202110210048426170b8ea884a9",
+                        "developpeur": "kaboretidiane"
+                    }
+                }
+
+            - response will be:
+                {
+                    "response_code": "00",
+                    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF9pbnZvaWNlIjoiMTU0NTkwMTIiLCJzdGFydF9kYXRlIjoiMjAyMy0wMi0yMyAxODoyNDoxMiIsImV4cGlyeV9kYXRlIjoxNjc3MjU5NDUyfQ.U9GwpBQpZwW1YdWD_0Zla9-Uy-sUV7zDn4Nobtfc33M",
+                    "response_text": "https://client.ligdicash.com/directpayment/invoice/   eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF9pbnZvaWNlIjoiMTU0NTkwMTIiLCJzdGFydF9kYXRlIjoiMjAyMy0wMi0yMyAxODoyNDoxMiIsImV4cGlyeV9kYXRlIjoxNjc3MjU5NDUyfQ.U9GwpBQpZwW1YdWD_0Zla9-Uy-sUV7zDn4Nobtfc33M",
+                    "description": "",
+                    "custom_data": {
+                        "transaction_id": "2021000000001",
+                        "logfile": "2023022318241263f7af4ccc792",
+                        "developpeur": "kaboretidiane",
+                    },
+                    "wiki": "https://client.ligdicash.com/wiki/createInvoice",
+                }
+        """
+        headers = {
+            "content-type": "application/json",
+            "Apikey": self._username,
+            "authorization": f"Bearer {self._password}",
+            "accept": "application/json",
+        }
+
+        response = requests.post(
+            self._url,
+            headers=headers,
+            json={"commande": command},
+            verify=verify_ssl,
+        )
+        return response.json()
+
+    @property
+    def url(self):
+        return self._url
+
+    @url.setter
+    def url(self, value):
+        self._url = value
+
+    @url.deleter
+    def url(self):
+        del self._url
+
+
+class Payment(GenericPaymentWithRedirect):
+    def __init__(self, phonenumber="", username="", password=""):
+        url = ligdicash_prod_url_with_redirect
+        super().__init__(url, phonenumber, username, password)
