02a6616147ed2230b4b4361298add8a5fce0dbce
Muhammed Zeba <parice02@hotmail.com>
2023-11-28 16:59:09 +0000

    update

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from mobilemoney import validate_ligdicash_payment
 
 today = datetime.now()
 
-trans_id = f"LDG{(today.year)}{today.month}{today.day}{today.hour}{today.minute}{today.second}{today.microsecond}C{randint(5, 100000)}"
+trans_id = f"LDG{(today.year)}{today.month}{today.day}.{today.hour}{today.minute}{today.second}.C{randint(5, 100000)}"
+
+# trans_id = f"LDG{(today.year)}{today.month}{today.day}{today.hour}{today.minute}{today.second}{today.microsecond}C{randint(5, 100000)}"
+
 
 command = {
     "invoice": {
@@ -21,11 +24,11 @@ command = {
         ],
         "total_amount": 100,
         "devise": "XOF",
-        "description": " Description du contenu de la facture(Achat de jus de fruits)",
-        "customer": "+22664712648",
-        "customer_firstname": "Nom du client",
-        "customer_lastname": "Prénom du client",
-        "customer_email": "tester@gligdicash.com",
+        "description": "Description du contenu de la facture(Achat de jus de fruits)",
+        # "customer": "+22664712648",
+        # "customer_firstname": "Nom du client",
+        # "customer_lastname": "Prénom du client",
+        # "customer_email": "tester@gligdicash.com",
         # "external_id": "",
         # "otp": "",
     },
@@ -40,15 +43,14 @@ command = {
     },
     "custom_data": {
         "transaction_id": trans_id,  # id à générer
-        # "logfile": "202110210048426170b8ea884a9",
-        # "developpeur": "kaboretidiane",
+        # "Plateforme": "E-TIMBRE",
     },
 }
 
 
 response = validate_ligdicash_payment(
-    api_key="TKC8O8BX9TAA8EL0G",  # SUGSFAKICRVXD1OSH
-    api_token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF9hcHAiOiIxMjgyMSIsImlkX2Fib25uZSI6MzUzNDYyLCJkYXRlY3JlYXRpb25fYXBwIjoiMjAyMy0xMS0yOCAwOToyMzo0MiJ9.zr8Vs1vZgkPSJIfjhvDfnEtGv2qZrvQvx4BKFGJKxP8",
+    api_key="SUGSFAKICRVXD1OSH",  # SUGSFAKICRVXD1OSH
+    api_token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF9hcHAiOiIxMjQ5NiIsImlkX2Fib25uZSI6MzUzNDYyLCJkYXRlY3JlYXRpb25fYXBwIjoiMjAyMy0xMS0xMCAxNDo1NTo0NCJ9.vlSBW5zvwMAAaupXZrZi5GRrf79Ctuka6Rz1Uprbf54",
     command=command,
 )
 print(response)
