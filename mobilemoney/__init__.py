1f81de9843a9bb766816f63c465e0090eec770dd
Muhammed Zeba <parice02@hotmail.com>
2024-07-06 23:42:15 +0000

    update

--- mobilemoney/__init__.py
+++ mobilemoney/__init__.py
@@ -45,7 +45,6 @@ def validate_moov_prod_payment(
     otp_trans_id: str,
     url,
 ):
-    print("validate_moov_prod_payment", otp_trans_id)
     payment = MMPayment(url, username, password)
     return payment.validate_payment(
         customer_phone, customer_otp, amount, message, otp_trans_id
@@ -63,7 +62,6 @@ def validate_moov_dev_payment(
     otp_trans_id,
     url=None,
 ):
-    print("validate_moov_dev_payment", otp_trans_id)
     payment = MMDevPayment(url, username, password)
     return payment.validate_payment(
         customer_phone, customer_otp, amount, message, otp_trans_id
